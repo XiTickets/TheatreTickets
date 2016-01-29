4caec601fe4c32c28b04c8ff055430983ba8682c
Jared Bates <computerwizjared@hotmail.com>
2016-01-29 10:57:34 -0600

    Add templates per http://serebrov.github.io/html/2012-08-20-expressjs-ejs-reuse-templates.html

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var templates = {
+    seatselection: fs.readFileSync('./views/pages/seatselection.ejs', 'utf-8'),
+    checkout: fs.readFileSync('./views/pages/checkout.ejs', 'utf-8')
+};
 
 router.get('/', function (req, res, next) {
-    res.render('index', {title: 'Forsyth Tickets'});
+    res.render('index', {
+        title: 'Forsyth Tickets',
+        templates: templates
+    });
 });
 
 module.exports = router;
